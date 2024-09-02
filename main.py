aa35c1e38db451bbe84c8695ad3190fc60b26696
vranghel <150031559+vranghel@users.noreply.github.com>
2024-09-02 12:38:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY", None)
 
-MAX_FILE_SIZE = 25 * 1024 * 1024  # 25 MB
+MAX_FILE_SIZE = 100 * 1024 * 1024  # 25 MB
 FILE_TOO_LARGE_MESSAGE = "The audio file is too large for the current size and rate limits using Whisper. If you used a YouTube link, please try a shorter video clip. If you uploaded an audio file, try trimming or compressing the audio to under 25 MB."
 
 audio_file_path = None
